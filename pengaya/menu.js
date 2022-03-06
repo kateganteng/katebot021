202f0f7ac9334aec71043547c5f7e571d49a0bf6
kateganteng <101029958+kateganteng@users.noreply.github.com>
2022-03-06 11:53:54 +0700

    Update menu.js

--- pengaya/menu.js
+++ pengaya/menu.js
@@ -9,27 +9,24 @@ const groups = chats.filter(v => v.jid.endsWith('g.us'))
 const defaultMenu = {
   before: `
 ┏━━〔 ${namabot} 〕━⬣
-┃• Hai, %name!
+┃⬡ Hai, %name!
 ┃
-┃• Tersisa *%limit Limit*
-┃• Role *%role*
-┃• Level *%level (%exp / %maxexp)* 
-┃• [%xp4levelup]
-┃• %totalexp XP secara Total
+┃⬡ Tersisa *%limit Limit*
+┃⬡ Role *%role*
+┃⬡ Level *%level (%exp / %maxexp)* 
+┃⬡ [%xp4levelup]
+┃⬡ %totalexp XP secara Total
 ┃ 
-┃• Hari : *%week %weton* 
-┃• Tanggal : *%date*
-┃• Tanggal Islam : 
-┃• *%dateIslamic*
-┃• Waktu: *%time*
+┃⬡ Hari : *%week %weton* 
+┃⬡ Tanggal : *%date*
+┃⬡ Tanggal Islam : 
+┃⬡ *%dateIslamic*
+┃⬡ Waktu: *%time*
 ┃
-┃• Uptime: *%uptime (%muptime)*
-┃• Database: %rtotalreg dari %totalreg
-┃• YT OWNER:
-┃• https://youtube.com/c/KATEARRAWAWIE
-┃
-┃• Instagram:
-┃• https://instagram.com/mursid.st
+┃⬡ Uptime: *%uptime (%muptime)*
+┃⬡ Database: %rtotalreg dari %totalreg
+┃⬡ YOUTUBE
+┃⬡ https://youtube.com/c/KATEARRAWAWIE
 ┃
 ┗━━━━━━⬣`.trimStart(),
   header: '┏━━〔 %category 〕━⬣',
@@ -243,13 +240,13 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
 ┃⬡ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
 ┃⬡ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
 ┃
-┃⬡ Script By Mursid
-┃⬡ Github Name : Katebot021
-┃⬡ Yt : https://youtube.com/c/KATEARRAWAWIE
+┃⬡ Script by Alyaa
+┃⬡ Github Name : AlyaaXd
+┃⬡ Yt : AlyaaXd
 ┃⬡ Run bot : Heroku
 ┃⬡ Tipe SC rfkbot
 ┗━━━━━━━━⬣`.trim(),
-          "buttonText":"Klik Disini",
+          "buttonText": "Klik Disini",
           "listType": "SINGLE_SELECT",
           "sections": [
             {
