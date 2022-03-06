3f6333c7b83fd0d2b94e71ab849594107d5af2f5
kateganteng <101029958+kateganteng@users.noreply.github.com>
2022-03-06 10:53:28 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ PULSA : 085730903853
-├ OVO & DANA : 085730903853
+├ OVO & DANA : 0859 5494 4195
+├ Owner : wa.me/62859 5494 4195
 └────
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
